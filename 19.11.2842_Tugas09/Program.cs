379ba33fccbe00a85a7a01415076fce50d925ef6
muhammad farhan <muhammad.2842@students.amikom.ac.id>
2023-07-15 18:06:35 +0700

    Tugas 09

---
+++ 19.11.2842_Tugas09/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _19._11._2842_Tugas09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrinterWindows printer;
+
+            Console.WriteLine("Pilih printer : ");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Canon");
+            Console.WriteLine("3. LaserJet");
+
+            Console.Write("Nomor printer [1..3] : ");
+            int nomorPrinter = Convert.ToInt32(Console.ReadLine());
+
+            if (nomorPrinter == 1)
+                printer = new Epson();
+            else if (nomorPrinter == 2)
+                printer = new Canon();
+            else if (nomorPrinter == 3)
+                printer = new LaserJet();
+            else
+                printer = new Salah();
+
+            printer.Show();
+            printer.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
